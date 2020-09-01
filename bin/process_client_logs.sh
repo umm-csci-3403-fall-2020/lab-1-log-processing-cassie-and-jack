129276b39dcf42a206d737378575a8b0217ed409
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-01 13:19:44 -0500

    I am still getting errors (but less) I am commiting because I want to eat lunch and close out of the terminal but there is an issues with the range I have set for the ask command. will come back to later

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,6 +6,6 @@ CLIENT_DIRECTORY="$1"
 
 cd "$CLIENT_DIRECTORY" || exit
 # Gathering all of the contents from the log files and piping them into extraction command
-cat var/log/* | awk  'match($0, ^(A-Za-Z]+)[ ]+([0-9]+ [0-9][0-9][A-Za-Z]+ /sshd\[[0-9]+\]: Failed password for /from/ ([0-9]+\.[0-9]+./[0-9]+./[0-9]+)/, groups ) ) {print groups[1], groups[2], groups[3], groups[4] }'  > failed_login_data.txt
+cat var/log/* | awk  'match($0, /^([a-zA-Z]+)[ ]+([0-9]+ [0-9][0-9]):[0-9]+ [a-zA-Z]+ sshd\[[0-9]+\]:Failed password for[A-Za-z]* ([A-Zz-A_0-9]+) from ([0-9]+.[0-9]+.[0-9]+.[0-9]+)/,groups) {print groups[1], groups[2], groups[3], groups[4]}'  > failed_login_data.txt
 
 
