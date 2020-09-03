07e6e04c7c82d1d51604380dd944066aedd48419
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-03 10:17:54 -0500

    The process_clients_logs.sh script is now passing the tests. I changed to a >> , > in order to append to the file instead of overwrite it

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,12 +6,12 @@ CLIENT_DIRECTORY="$1"
 
 cd "$CLIENT_DIRECTORY" || exit
 # Gathering all of the contents from the log files and piping them into extraction command
-cat "$CLIENT_DIRECTORY"/var/log/* |\
+cat *var/log/* |\
 	awk '/^([A-Za-z]+)/ && $6~/Failed/ && $9~/invalid/ {print $1, $2, $3, $11, $13}' \
-	| sed -e 's/\(..\):\(..\):\(..\)//g' |tee failed_login_data.txt
+	| sed -e 's/\:\(..\):\(..\)//g' > failed_login_data.txt
 
 # awk for failed login attempts that are actual user names in the lab
-cat "$CLIENT_DIRECTORY"/var/log/* |\
+cat *var/log/* |\
  	awk '/^([A-Za-z]+)/ && $6~/Failed/ && $10~/from/ {print $1, $2, $3, $9, $11}' \
-	| sed -e 's/\(..\):\(..\):\(..\)//g' |tee failed_login_data.txt 
+	| sed -e 's/\:\(..\):\(..\)//g' >> failed_login_data.txt 
 
