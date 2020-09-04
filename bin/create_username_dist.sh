e520e09e012dcd1c25ba6c9634ab122c69bd9790
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-03 22:47:02 -0500

    I have created a script for the create usernames that passes all current tests. I used an awk command to pipe usernames into a sort, then into a uniq -c to count the lines I also made an adjustment to the wrap_contents.sh, as there seemed to be some errors when the script was moved

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#***********CONSTANTS
 DIRECTORY_NAME=$1
-
+HERE=$(pwd) 
+#************MAIN
 cd "$DIRECTORY_NAME" || exit
-ls
-#cat */failed_login_data.txt \
-	#| awk '/^([A-Za-z]+)/ && $6~/Failed/ && $9~/invalid/ {print $11}' > username_dist.html
 
-cat testInput.txt \
-	| sort -u | uniq -c > testOutput.txt
+cat ./*/failed_login_data.txt \
+	| awk ' {print $4} ' | sort | uniq -c | awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"}' \
+	> temp_username_dist.html
+
+# Using wrap contentssh to add footer and header to usernames
+cd "$HERE" || exit
+./bin/wrap_contents.sh "$DIRECTORY_NAME"/temp_username_dist.html html_components/username_dist \
+	"$DIRECTORY_NAME"/username_dist.html
+
+# clean up temp files
+rm "$DIRECTORY_NAME"/temp_username_dist.html
+
+
+
+
