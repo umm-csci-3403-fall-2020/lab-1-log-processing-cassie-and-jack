1d1d7b13c6743b429bc710385e6b3290cc846ecc
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-04 09:25:40 -0500

    added comments, and checked existing comments for clarity

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,6 +5,8 @@ HERE=$(pwd)
 #************MAIN
 cd "$DIRECTORY_NAME" || exit
 
+# take the usernames from failed_login_data.txt, sort, and count occurances of each username
+# second awk command then prints desired output into code that will create pie chart
 cat ./*/failed_login_data.txt \
 	| awk ' {print $4} ' | sort | uniq -c | awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"}' \
 	> temp_username_dist.html
