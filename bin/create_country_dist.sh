9517105d8d8de7496a6465d0a12864ec91efeac5
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-04 22:20:28 -0500

    I have gotten it so that the correct awk and join v=commands, however I am having troubles with the wrap_contents.sh now.

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -4,25 +4,32 @@
 DIRECTORY_NAME=$1
 HERE=$(pwd)
 #************MAIN
-cd "$DIRECTORY_NAME" || exit
+#cd "$DIRECTORY_NAME" || exit
 
 # Extracting the appropriate column to grab the hour using awk
-cat ./*/failed_login_data.txt \
-	| awk ' {print $5} ' | sort > temp_country_IP.html
-sort "$HERE"/etc/country_IP_map.txt
-       	join -1 2 -2 2 temp_country_IP.html "$HERE"/etc/country_IP_map.txt > temp2_country_IP.html 
-cat temp2_country_IP.html | awk '{print $2}' | sort | uniq -c\
-	| awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"}' \
-	> temp_country_dist.html
+#cat ./*/failed_login_data.txt \
+	#| awk ' {print $5} ' | sort > temp_country_IP.html
 
-
+#join temp_country_IP.html "$HERE"/etc/country_IP_map.txt |\
+	# awk '{print $2}' | sort | uniq -c |\
+       	#awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"}' >  temp_country_dist.html
 
 # Using wrap contentssh to add footer and header to usernames
-cd "$HERE" || exit
+#cd "$HERE" || exit
 
-./bin/wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist \
-	"$DIRECTORY_NAME"/country_dist.html
+
+#./bin/wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist \
+	#"$DIRECTORY_NAME"/country_dist.html
 
 # Clean up temp files
 #rm "$DIRECTORY_NAME"/temp_country_dist.html
+cd "$DIRECTORY_NAME" || exit
+cat ./*/failed_login_data.txt | awk '{print $5}' | sort > "$HERE"/temp_check.txt 
+join "$HERE"/temp_check.txt "$HERE"/etc/country_IP_map.txt | awk '{print $2} '| uniq -c \
+	| awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"} ' > temp_country_dist.html
+
+cd "$HERE"/bin || exit
+
+./wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist "$DIRECTORY_NAME"/country_dist.html
+
 
