4286b1d16d17fdc7455584aeef233d6ecc7d88a1
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-05 00:26:21 -0500

    I fixed the issues in the create country script.  I was neglecting to sort the input before it was being piped into the uniq command, as a result of this I was getting repeats in the final output.  I have fixed this issue by adding a sort in before the uniq -c counts the lines

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,35 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #***********CONSTANTS
 DIRECTORY_NAME=$1
 HERE=$(pwd)
 #************MAIN
-#cd "$DIRECTORY_NAME" || exit
 
-# Extracting the appropriate column to grab the hour using awk
-#cat ./*/failed_login_data.txt \
-	#| awk ' {print $5} ' | sort > temp_country_IP.html
-
-#join temp_country_IP.html "$HERE"/etc/country_IP_map.txt |\
-	# awk '{print $2}' | sort | uniq -c |\
-       	#awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"}' >  temp_country_dist.html
-
-# Using wrap contentssh to add footer and header to usernames
-#cd "$HERE" || exit
-
-
-#./bin/wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist \
-	#"$DIRECTORY_NAME"/country_dist.html
-
-# Clean up temp files
-#rm "$DIRECTORY_NAME"/temp_country_dist.html
 cd "$DIRECTORY_NAME" || exit
 cat ./*/failed_login_data.txt | awk '{print $5}' | sort > "$HERE"/temp_check.txt 
-join "$HERE"/temp_check.txt "$HERE"/etc/country_IP_map.txt | awk '{print $2} '| uniq -c \
-	| awk ' { print "data.addRow( [\x27"$2"\x27, "$1"]);"} ' > temp_country_dist.html
-
-cd "$HERE"/bin || exit
-
-./wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist "$DIRECTORY_NAME"/country_dist.html
+join "$HERE"/temp_check.txt "$HERE"/etc/country_IP_map.txt | awk '{print $2} '| sort | uniq -c \
+	| awk ' { print "data.addRow([\x27"$2"\x27, "$1"]);"} ' > temp_country_dist.html
 
 
+cd "$HERE" || exit
+./bin/wrap_contents.sh "$DIRECTORY_NAME"/temp_country_dist.html html_components/country_dist "$DIRECTORY_NAME"/country_dist.html
+
+# clean up temp files
+rm "$DIRECTORY_NAME"/temp_country_dist.html
+rm "$HERE"/temp_check.txt
