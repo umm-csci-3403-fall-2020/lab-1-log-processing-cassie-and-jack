29463e9daa7866b73f64bbeb0720b23d121c8574
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-08 12:03:37 -0500

    We have created a for loop, and outside the for looop we are calling all of the previously created files. We are getting an error on calling the create_hours_dist.sh from the script but I am commiting now so all group members can debug if wanted.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ###### CONSTANTS ########
-
+HERE=$(pwd)
 # Target directories for decompressing
 DIR_1="$1"
 DIR_2="$2"
@@ -11,3 +11,21 @@ DIR_4="$4"
 
 # Setting up the scratch directory to extract the file contents to
 SCRATCH=$(mktemp -d)
+
+
+for d in "$@" ;do
+	BASENAME=$(basename -s .tgz "$d")
+	mkdir "$SCRATCH"/"$BASENAME"
+	tar -xzf "$d" -C  "$SCRATCH"/"$BASENAME"
+        ./bin/process_client_logs.sh "$SCRATCH"/"$BASENAME"
+		
+done	  
+	
+./bin/create_username_dist.sh "$SCRATCH"
+./bin/create_hours_dist.sh "$SCRATCH"
+#./bin/create_country_dist.sh "$SCRACTH"
+#./bin/assemble_reports.sh "$SCRATCH"
+
+
+
+
